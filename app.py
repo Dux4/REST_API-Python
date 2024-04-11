0ec897cf40a152176dec2218d61e8d50eecd57ba
eduardopassos55551@gmail.com <eduardopassos55551@gmail.com>
2024-04-10 22:19:36 -0300

    Iniciando Projeto Flask - Add GET, PUT, DELETE

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
 desenvolvedores = [
@@ -11,10 +11,19 @@ desenvolvedores = [
      }
 ]
 
-@app.route('/dev<int:id>')
+@app.route('/dev/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def desenvolvedor(id):
-    desenvolvedor=desenvolvedores[id] #retorna a posicao do vetor
-    return jsonify(desenvolvedor)
+    if request.method == 'GET':
+        desenvolvedor=desenvolvedores[id] #retorna a posicao do vetor
+        print(desenvolvedor)
+        return jsonify(desenvolvedor)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status': 'sucesso', 'mensagem' : 'Registro Deletado'})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
